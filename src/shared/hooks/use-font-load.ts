06bc21ed7d37ce6550b09b7f856b7950f1794a4d
Ilya Makarov <feesler@gmail.com>
2025-11-16 06:51:40 +0300

    * Closed #17: Added useFontLoad() hook to control font loading state

---
+++ src/shared/hooks/use-font-load.ts
@@ -0,0 +1,51 @@
+import { StoreProviderContext } from '@jezvejs/react';
+import { useEffect } from 'react';
+import { AppState } from 'shared/types.ts';
+import { actions } from 'store/reducer.ts';
+
+export function useFontLoad(context: StoreProviderContext<AppState>) {
+  const { getState, dispatch } = context;
+
+  const waitForFontLoad = async () => (
+    new Promise((resolve) => {
+      const checkState = () => {
+        const st = getState();
+
+        if (st.fontLoaded) {
+          resolve(true);
+        } else {
+          setTimeout(checkState, 50);
+        }
+      };
+
+      checkState();
+    })
+  );
+
+  useEffect(() => {
+
+    const loadFont = async () => {
+      const st = getState();
+      if (st.fontLoaded || st.fontLoading) {
+        return;
+      }
+
+      dispatch(actions.setFontLoading(true));
+
+      const font = new FontFace('code', 'url("assets/code.woff")');
+      await font.load();
+      document.fonts.add(font);
+
+      dispatch(actions.setFontLoading(false));
+      dispatch(actions.setFontLoaded());
+
+      await waitForFontLoad();
+    };
+
+    loadFont();
+  }, []);
+
+  const st = getState();
+
+  return st.fontLoaded;
+}
